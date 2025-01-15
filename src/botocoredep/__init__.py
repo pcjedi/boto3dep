01f58bd6b7d1cd1d628b79b3c13b635e7bbdf4dc
Georg Wendorf <georgfriedrich.wendorf@freenet.ag>
2025-01-15 11:54:00 +0100

    d

--- src/botocoredep/__init__.py
+++ src/botocoredep/__init__.py
@@ -3,12 +3,7 @@ import botocore
 
 def example():
     """
-    >>> import botocore.session
-    >>> session = botocore.session.get_session()
-    >>> client = session.create_client('ec2')
-    >>> print(client.describe_instances())
-    Traceback (most recent call last):
-      ...
-    botocore.exceptions.NoRegionError: You must specify a region.
+    >>> 1 + 1
+    2
     """
     return botocore
