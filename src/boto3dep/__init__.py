165a3fbb8332269b38be353ca917b90d8aac537c
Georg Wendorf <georgfriedrich.wendorf@freenet.ag>
2025-01-15 11:13:43 +0100

    Pcjedi-system-python (#3)
    * d
    
    * d
    
    * d
    
    * d

--- src/boto3dep/__init__.py
+++ src/boto3dep/__init__.py
@@ -1,6 +1,4 @@
 import boto3
-import dateutil
-from six import PY3, string_types
 
 
 def get_s3_client():
@@ -10,12 +8,3 @@ def get_s3_client():
     <botocore.client.S3 object at 0x...>
     """
     return boto3.client("s3")
-
-
-def parse_date(date_str):
-    """
-    Parse a date string
-    >>> parse_date("2018-01-01")
-    datetime.datetime(2018, 1, 1, 0, 0)
-    """
-    return dateutil.parser.parse(date_str)
